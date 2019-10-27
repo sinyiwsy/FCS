c2c9023a5f14af4f8bd07eb9e8eae6a4562e32f5
kevin909cty <cheungkevinviola909@gmail.com>
2019-10-28 00:45:06 +0800

    three, four

--- src/four.tsx
+++ src/four.tsx
@@ -1,8 +1,139 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, Theme, createStyles} from '@material-ui/core/styles';
+
+import { red } from '@material-ui/core/colors';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import PaymentIcon from '@material-ui/icons/Payment';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import MobileScreenShareIcon from '@material-ui/icons/MobileScreenShare';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles2 = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      maxWidth: 376,
+      maxHeight: 667,
+    },
+    media: {
+      height: 0,
+      paddingTop: '40%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }),
+);
+
+const useStyles = makeStyles(theme => ({
+    avatar: {
+      margin: 10,
+      width: 60,
+      height: 60,
+    },
+    list: {
+      marginBottom: theme.spacing(2),
+    },
+    subheader: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    image: {
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+  }));
 
 export const Four = () => {
+    const classes2 = useStyles2();
+    const classes = useStyles();
+
     return(
-        <h6>Four Page</h6>
+        <div>
+            <Box position="static" color="primary" height="70px" style={{ backgroundImage: 'url(https://digitalsynopsis.com/wp-content/uploads/2017/07/beautiful-color-ui-gradients-backgrounds-peach.png)'}}>
+                <Toolbar variant="dense" >
+                    <Box p={2} mx={0} my={-2.3}>
+                    <h4>Config Payment</h4>
+                    </Box>
+                </Toolbar>
+            </Box>
+
+            <Box position="static" color="primary" height="70px" p={3} mx={2} my={-2}>
+            <h4>Payment - Latte Art Workshop</h4>
+                <FormControl >
+                    <InputLabel htmlFor="input-with-icon-adornment">Base Charges</InputLabel>
+                        <Input id="input-with-icon-adornment"
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <PaymentIcon />
+                                </InputAdornment>
+                            }
+                        />
+                </FormControl>
+                <h4></h4>
+                <FormControl >
+                    <InputLabel htmlFor="input-with-icon-adornment">Deposit</InputLabel>
+                        <Input id="input-with-icon-adornment"
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <AttachMoneyIcon />
+                                </InputAdornment>
+                            }
+                        />
+                </FormControl>
+                <h4></h4>
+                <FormControl >
+                    <InputLabel htmlFor="input-with-icon-adornment">Settlement</InputLabel>
+                        <Input id="input-with-icon-adornment"
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <MobileScreenShareIcon />
+                                </InputAdornment>
+                            }
+                        />
+                </FormControl>
+            </Box>
+
+            <Box color="primary" marginTop="270px" height="240px"  style={{ backgroundImage: 'url(https://i.pinimg.com/originals/9c/ce/d3/9cced3264a1e7b144467f506923ebb0b.jpg)'}}>
+            <TextField 
+                label="Discount Code"
+                style={{ marginTop: -10, marginLeft: 39}}
+                variant="outlined"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                />
+            <Box color="primary" p={0} mx={6} my={-3}>
+                <Link to="/four">
+                    <h6>+ Further Discount Code</h6>
+                </Link>
+            </Box>
+            <Box  p={0} mx={10} my={10}>
+            <Button variant="contained">Process Payment</Button>
+            </Box>
+            </Box>
+
+        </div>
     )
 }
--- src/three.tsx
+++ src/three.tsx
@@ -1,8 +1,127 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, Theme, createStyles} from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles2 = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      maxWidth: 376,
+      maxHeight: 667,
+    },
+    media: {
+      height: 0,
+      paddingTop: '40%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }),
+);
+
+const useStyles = makeStyles(theme => ({
+    avatar: {
+      margin: 10,
+      width: 60,
+      height: 60,
+    },
+    list: {
+      marginBottom: theme.spacing(2),
+    },
+    subheader: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    image: {
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+  }));
 
 export const Three = () => {
+    const classes2 = useStyles2();
+    const classes = useStyles();
+    
     return(
-        <h6>Three Page</h6>
+        <div>
+            <Box position="static" color="primary" height="250px" style={{ backgroundImage: 'url(https://digitalsynopsis.com/wp-content/uploads/2017/07/beautiful-color-ui-gradients-backgrounds-peach.png)'}}>
+                <Toolbar variant="dense" >
+                    <Box p={2} mx={3} my={3}>
+                        <React.Fragment>
+                            <Avatar alt="Profile Picture" src="https://d22ir9aoo7cbf6.cloudfront.net/wp-content/uploads/sites/6/2018/11/gifts-for-men-in-Hong-Kong-MAIN-IMAGE-1024x802.jpg" className={classes.avatar}></Avatar>
+                            <Typography>Recent Deposits</Typography>
+                            <Typography variant="h6">$3,024.00</Typography>
+                            <Typography color="textSecondary">on 15 March, 2019</Typography>
+                        </React.Fragment>
+                    </Box>
+                </Toolbar>
+            </Box>
+
+            <Card className={classes2.card}>
+                <CardHeader avatar={
+                    <Avatar aria-label="recipe" className={classes2.avatar}>
+                    R
+                    </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title="Latte Art Workshop"
+                    subheader="September 14, 2019"
+                    />
+            <Link to="/four">
+            <CardMedia
+                className={classes2.media}
+                image="https://academiedecafedemontreal.com/wp-content/uploads/2016/12/acm_siteweb_latteart01-1000x667.jpg"
+                title="Paella dish"
+            />
+            </Link>
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="h6">
+                Any individual bookings exceeding the max. capacity are to be quoted separately as a Private Event.
+                </Typography>
+            </CardContent>
+                <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </div>
+
+
     )
 }
